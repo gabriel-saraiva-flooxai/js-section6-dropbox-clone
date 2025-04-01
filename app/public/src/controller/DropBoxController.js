9dd8c1f760d4e93dfeb7a328927f11e78254a327
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-01 18:14:06 -0300

    feat: open file

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -717,7 +717,7 @@ class DropBoxController {
                     break;
 
                 default:
-                    window.open('/file?path=' + file.path);
+                    window.open(file.path);
 
             }
 
